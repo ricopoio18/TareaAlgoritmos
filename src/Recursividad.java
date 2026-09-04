2d3860a741b8afe5cc0ac6cb83788cfda585f935
Josué <josue.rico@uabc.edu.mx>
2026-09-04 15:51:21 -0700

    Método caracterApareceAntes()

--- src/Recursividad.java
+++ src/Recursividad.java
@@ -1,3 +1,13 @@
 
 public class Recursividad {
+
+    public static boolean caracterApareceAntes(String cad, char c1, char c2){
+        if (cad == null || cad.length() < 2) {
+            return false;
+        }
+        if (cad.charAt(0) == c1 && cad.charAt(1) == c2) {
+            return true;
+        }
+        return caracterApareceAntes(cad.substring(1), c1, c2);
+    }
 }
