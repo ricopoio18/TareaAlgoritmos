e155a93ae0271ea286d0c3aaab221eaff815b65a
Josué <josue.rico@uabc.edu.mx>
2026-09-04 16:51:19 -0700

    El método se eliminó por accidente

--- src/Recursividad.java
+++ src/Recursividad.java
@@ -15,6 +15,16 @@ public class Recursividad {
         return suma + localizarYSumar(cadena.substring(1));
     }
 
+    public static boolean caracterApareceAntes(String cad, char c1, char c2){
+        if (cad == null || cad.length() < 2) {
+            return false;
+        }
+        if (cad.charAt(0) == c1 && cad.charAt(1) == c2) {
+            return true;
+        }
+        return caracterApareceAntes(cad.substring(1), c1, c2);
+    }
+
     public static int buscarXEnCadena(int[] cadenaNumeros, int numeroAEncontrar, int posicion){
         if(cadenaNumeros.length == posicion) {
             return -1;
