90ebe9f4b55fdaa366c035f1a17fcc576514bdc9
johab <johab.montoya@uabc.edu.mx>
2026-09-04 17:14:51 -0700

    Se agrega metodo orejas conejo

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] cadenaNumeros = {1,2,3,4};
-        //System.out.println(Recursividad.caracterApareceAntes("Parangaricutirimicuaro", 'u', 'c'));
-        System.out.println(Recursividad.localizarYSumar("Nuevo Leon 298"));
+        int[] cadenaNumeros = {1,8,3,4};
         System.out.println(Recursividad.buscarXEnCadena(cadenaNumeros, 4, 0));
+        System.out.println(Recursividad.valorMasGrandeArreeglo(cadenaNumeros, 0, 0));
+        System.out.println(Recursividad.caracterApareceAntes("Parangaricutirimicuaro", 'u', 'c'));
+        System.out.println(Recursividad.cantidadOrejasConejo(70,0));
+        System.out.println(Recursividad.localizarYSumar("Nuevo Leon 298"));
     }
 }
--- src/Recursividad.java
+++ src/Recursividad.java
@@ -1,6 +1,25 @@
 
 public class Recursividad {
-    
+
+
+    public static int buscarXEnCadena(int[] cadenaNumeros, int numeroAEncontrar, int posicion){
+        if(cadenaNumeros.length == posicion) {
+            return -1;
+        } else if(cadenaNumeros[posicion] == numeroAEncontrar){
+            return numeroAEncontrar;
+        }
+        return buscarXEnCadena(cadenaNumeros, numeroAEncontrar,posicion + 1);
+    }
+
+    public static int valorMasGrandeArreeglo(int[] cadenaNumeros, int posicion, int numeroMasGrande){
+        if(posicion == cadenaNumeros.length){
+            return numeroMasGrande;
+        }
+        if (cadenaNumeros[posicion] > numeroMasGrande){
+            numeroMasGrande = cadenaNumeros[posicion];
+        }
+        return valorMasGrandeArreeglo(cadenaNumeros,posicion + 1, numeroMasGrande);
+    }
 
     public static int localizarYSumar(String cadena){
 
@@ -15,6 +34,13 @@ public class Recursividad {
         return suma + localizarYSumar(cadena.substring(1));
     }
 
+    public static int cantidadOrejasConejo(int cantidadConejos, int cantidadOrejas){
+        if(cantidadConejos == 0){
+            return cantidadOrejas;
+        }
+        return cantidadOrejasConejo(cantidadConejos-1, cantidadOrejas + 2);
+    }
+
     public static boolean caracterApareceAntes(String cad, char c1, char c2){
         if (cad == null || cad.length() < 2) {
             return false;
@@ -23,14 +49,5 @@ public class Recursividad {
             return true;
         }
         return caracterApareceAntes(cad.substring(1), c1, c2);
-    }
-
-    public static int buscarXEnCadena(int[] cadenaNumeros, int numeroAEncontrar, int posicion){
-        if(cadenaNumeros.length == posicion) {
-            return -1;
-        } else if(cadenaNumeros[posicion] == numeroAEncontrar){
-            return numeroAEncontrar;
-        }
-        return buscarXEnCadena(cadenaNumeros, numeroAEncontrar,posicion + 1);
     }
 }
